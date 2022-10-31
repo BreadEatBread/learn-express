6ca2164950ba4bac2461e0610af18e63c2d82eb7
BreadEatBread <woong4252@gmail.com>
2022-10-31 12:16:35 +0900

    10.31.22 12:20

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello, Express');
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
